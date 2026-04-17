63c9020d32989d21f013d3532842218df0fc6c3e
divyesh-12 <divyeshchavda625@gmail.com>
2026-04-17 14:02:50 +0530

    Add files via upload

---
+++ Full-stack-development/conditionals task/script.js
@@ -0,0 +1,12 @@
+// javascript conditional parctice questions
+
+// esay
+// 1.
+let num = 25;
+ if (num > 0) {
+    console.log("positive");
+ } else if (num == 0) {
+    console.log("zero");
+ } else {
+    console.log("negative");
+}
